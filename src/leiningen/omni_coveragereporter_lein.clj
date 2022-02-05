942d137ba41b68583088a906d3635df49b200179
Joao Esperancinha <jofisaes@gmail.com>
2022-02-05 17:05:49 +0100

    🌿 Pipeline update

--- src/leiningen/omni_coveragereporter_lein.clj
+++ src/leiningen/omni_coveragereporter_lein.clj
@@ -3,4 +3,7 @@
 (defn omni-coveragereporter-lein
   "I don't do a lot."
   [project & args]
-  (println "Hi!"))
+  (println "Hi!")
+  (.getCanonicalPath (clojure.java.io/file "."))
+  (println(.getCanonicalPath (clojure.java.io/file ".")))
+  )
