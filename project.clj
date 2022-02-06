a1a4c811635e3c2f58c3db6c14dbf5295b818297
Joao Esperancinha <jofisaes@gmail.com>
2022-02-06 14:44:59 +0100

    🌿 Pipeline Updates

--- project.clj
+++ project.clj
@@ -5,11 +5,12 @@
   :license {:name "EPL-2.0 OR GPL-2.0-or-later WITH Classpath-exception-2.0"
             :url  "https://www.eclipse.org/legal/epl-2.0/"}
   :dependencies [[org.clojure/clojure "1.10.3"]]
-  :eval-in-leiningen false
+  ;:eval-in-leiningen false
   :repl-options {:init-ns test.core}
   :profiles {
-             :plugin   {:dependencies      [[org.clojure/clojure "1.10.3"]]
-                        :eval-in-leiningen true}
+             :run      {:dependencies      [[org.clojure/clojure "1.10.3"]]
+                        :eval-in-leiningen true
+                        :plugins           [[omni-coveragereporter-lein "0.0.0-SNAPSHOT"]]}
              :coverage {:dependencies      [[org.clojure/clojure "1.10.3"]]
                         :eval-in-leiningen false
                         :plugins           [[lein-cloverage "1.2.2"]]
