148281f5b0fd4d73e3a6f3e2ff549e47758fe92e
Joao Esperancinha <jofisaes@gmail.com>
2022-02-06 17:46:33 +0100

    🌿 Configuration File reading

--- src/leiningen/omni_coveragereporter_lein.clj
+++ src/leiningen/omni_coveragereporter_lein.clj
@@ -1,20 +1,22 @@
 (ns leiningen.omni-coveragereporter-lein
   (:require [cheshire.core :as json]
             [clojure.core]
-            [clojure.java.io :as io]))
+            [clojure.java.io :as io])
+  (:import (java.io FileReader)))
 
 (def banner (io/resource
               "banner.txt"))
 
-(def text (json/encode {:message "hello omni!"}))
-
 (defn omni-coveragereporter-lein
   "Omni Coverage Plugin For Leiningen Repos.
   Check https://leiningen.org/ for further details on how to use these repos"
-  [_ & args]
-  ;(.getCanonicalPath (clojure.java.io/file "."))
-  ;(println (.getCanonicalPath (clojure.java.io/file ".")))
+  [_ & _]
+  (println (.getCanonicalPath (clojure.java.io/file ".", "omni-config.json")))
   (println (slurp banner))
-  (println (apply str text))
-  (println (apply str args)))
+  (println "* Starting reporting...")
+  (let [config-file (.getCanonicalPath (clojure.java.io/file ".", "omni-config.json"))
+        configuration (slurp (FileReader. config-file))]
+        (println config-file)
+        (println configuration))
+  (println "* Reporting Finished!"))
 
